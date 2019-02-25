b8173ac573664edb1ed09ac9c54c09767c1c951e
DESKTOP-6E2KC2A\Samue <sbaird@stetson.edu>
2019-02-25 13:29:52 -0500

    Cleaned up code and fixed a hand initialization bug

--- Scripts/index.js
+++ Scripts/index.js
@@ -5,7 +5,6 @@ let discard = document.getElementById("buttons").children[1];
 let discardPile = [];
 let turn = 0;
 let winC = 0;
-
 class Card {
     constructor(element, suit, num) {
         this.suit = suit;
@@ -14,6 +13,14 @@ class Card {
         this.img = element;
         img.src = num + suit + '.png';
         
+    }
+
+    constructor(element) {
+        this.suit = "";
+        this.num = 0;
+        this.selected = false;
+        this.img = element;
+        draw();
     }
 
     onclick = () => {
@@ -29,15 +36,11 @@ class Card {
     draw() {
         num = Math.floor((Math.random() * 13) + 2);
 
-        let rand = Math.floor((Math.random() * 4) + 1);
-        if(rand === 1) {
-            suit = 'c';
-        } else if (rand === 2) {
-            suit = 'd';
-        } else if (rand === 3) {
-            suit = 's';
-        } else if (rand === 4) {
-            suit = 'h';
+        switch(Math.floor((Math.random() * 4) + 1)) {
+            case 1: suit = 'c';
+            case 2: suit = 'd';
+            case 3: suit = 's';
+            case 4: suit = 'h';
         }
 
         if(discardPile.includes("" + num + suit)) {
@@ -62,66 +65,38 @@ start.onclick = () => {
     if(start.innerHTML === "restart") {
         window.reload();
     } else {
-        start.innerHTML = "restart";
-
-        for(let user of document.getElementById("user").children){
-        
-            let suit = "";
-            let rand = Math.floor((Math.random() * 4) + 1);
-            if(rand === 1) {
-                suit = 'c';
-            } else if (rand === 2) {
-                suit = 'd';
-            } else if (rand === 3) {
-                suit = 's';
-            } else if (rand === 4) {
-                suit = 'h';
-            }
-    
-            userHand.push(new Card(user, suit, Math.floor((Math.random() * 13) + 2)));
+        for(let user of document.getElementById("user").children){;
+            userHand.push(new Card(user));
         }
     
         for(let ai of document.getElementById("ai").children){
-            
-            let suit = "";
-            let rand = Math.floor((Math.random() * 4) + 1);
-            if(rand === 1) {
-                suit = 'c';
-            } else if (rand === 2) {
-                suit = 'd';
-            } else if (rand === 3) {
-                suit = 's';
-            } else if (rand === 4) {
-                suit = 'h';
-            }
-    
-            aiHand.push(new Card(ai, suit, Math.floor((Math.random() * 13) + 2)));
+            aiHand.push(new Card(ai));
         }
+
+        start.innerHTML = "restart";
     }
+
+    
+
 }
 
 discard.onclick = () => {
     let discarded = false;
-    if( turn % 2 === 0)
-    {
+    if(turn % 2 === 0) {
         for(let card of userHand) {
-            if (card.discard()) {
-                discarded = true;
-            }
+            if (card.discard()) discarded = true;
         }
         turn++;
-    } else if (turn % 2 === 1) {
+    } else {
         for(let card of aiHand) {
-            if(card.discard()) {
-                discarded = true;
-            }
+            if(card.discard()) discarded = true;
         }
         turn++;
     }
 
-    if(discarded){
+    if(!discarded){
         winC++;
-        if(turn === 2){
+        if(winC === 2){
             document.getElementById("title").innerHTML = "You Won!";
             document.getElementById("title").style.color = 'green';
         }
